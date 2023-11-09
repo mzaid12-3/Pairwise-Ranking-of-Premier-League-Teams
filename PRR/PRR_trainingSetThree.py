5a3e3858ebc758c3985823cefd6e5b4cbdf40f3a
mzaid12-3 <73778548+mzaid12-3@users.noreply.github.com>
2023-11-09 22:33:02 +0200

    Add files via upload

---
+++ PRR/PRR_trainingSetThree.py
@@ -0,0 +1,242 @@
+from keras.src.utils import to_categorical
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow.keras.layers import MultiHeadAttention, LayerNormalization, Add, Dropout
+from kerastuner import HyperModel, RandomSearch
+
+from tensorflow.keras.layers import Input, Embedding, Dense, Concatenate
+import numpy as np
+from creatingPairsForTrainingSetThree import trainingSetOne
+from tensorflow.keras.utils import to_categorical
+import pandas as pd
+import numpy as np
+
+combined_pairs_trainingSetOne, combined_labels_trainingSetOne, _, team_pairs = trainingSetOne()
+X = combined_pairs_trainingSetOne
+X = X.reshape(1140, -1)
+# Parameters for the transformer block
+d_model = X.shape[1]  # Dimension of the input vectors
+
+
+# num_heads = 4  # Number of attention heads
+# ff_dim = 256  # Hidden layer size in feed forward network inside transformer
+# dropout_rate = 0.1  # Dropout rate
+# num_transformer_blocks = 2  # Number of transformer blocks
+
+# Define a Transformer block
+def transformer_block(inputs, num_heads, ff_dim, dropout_rate):
+    # Add a sequence dimension
+    inputs = tf.expand_dims(inputs, 1)
+
+    # Multi-head Self Attention
+    attention_output = MultiHeadAttention(num_heads=num_heads, key_dim=d_model)(inputs, inputs)
+    attention_output = Dropout(dropout_rate)(attention_output)
+    out1 = Add()([inputs, attention_output])
+    out1 = LayerNormalization(epsilon=1e-6)(out1)
+
+    # Feed-forward network
+    ffn_output = Dense(ff_dim, activation='relu')(out1)
+    ffn_output = Dense(inputs.shape[-1])(ffn_output)  # Set the output dimension to match the input dimension
+    ffn_output = Dropout(dropout_rate)(ffn_output)
+    out2 = Add()([out1, ffn_output])
+    out2 = LayerNormalization(epsilon=1e-6)(out2)
+
+    # Remove the sequence dimension
+    out2 = tf.squeeze(out2, 1)
+
+    return out2
+
+
+class TransformerDualInputHyperModel(HyperModel):
+    def __init__(self, input_shape1, input_shape2):
+        self.input_shape1 = input_shape1
+        self.input_shape2 = input_shape2
+
+    def build(self, hp):
+        input1 = tf.keras.Input(shape=self.input_shape1, name="input1")
+        input2 = tf.keras.Input(shape=self.input_shape2, name="input2")
+
+        flattened_input1 = tf.keras.layers.Flatten()(input1)  # Flattening the player pairs
+
+        merged_input = tf.keras.layers.Concatenate(axis=-1)([flattened_input1, input2])
+
+        # Hyperparameters
+        # num_heads = 4  # Number of attention heads
+        # ff_dim = 256  # Hidden layer size in feed forward network inside transformer
+        # dropout_rate = 0.1  # Dropout rate
+        # num_transformer_blocks = 2  # Number of transformer blocks
+
+        num_heads = hp.Int('num_heads', 2, 4, step=2)
+        ff_dim = hp.Int('ff_dim', 128, 256, step=64)
+        dropout_rate = hp.Float('dropout_rate', 0.0, 0.2, step=0.1)
+        num_transformer_blocks = hp.Int('num_transformer_blocks', 1, 2)
+
+        x = merged_input
+        for _ in range(num_transformer_blocks):
+            x = transformer_block(x, num_heads, ff_dim, dropout_rate)
+
+        # Here, you can add your output layers based on the task, for example:
+        outputs = tf.keras.layers.Dense(30, activation='softmax')(x)  # Since there are 30 classes
+
+        model = tf.keras.Model(inputs=[input1, input2], outputs=outputs)
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+        return model
+
+
+# Assume X_train is your input data
+encoded_labels = to_categorical(combined_labels_trainingSetOne, num_classes=30)
+team_pairs = np.array(team_pairs)
+
+(train_pairs, val_pairs, train_teams, val_teams, train_labels, val_labels) = train_test_split(
+    combined_pairs_trainingSetOne,
+    team_pairs,  # Include team_pairs in the split
+    encoded_labels,
+    test_size=0.1,
+    random_state=42
+)
+
+input_shape1 = (2, 51)
+input_shape2 = (2,)
+dual_hypermodel = TransformerDualInputHyperModel(input_shape1, input_shape2)
+
+tuner = RandomSearch(
+    dual_hypermodel,
+    objective='val_accuracy',
+    max_trials=5,
+    executions_per_trial=1
+)
+
+tuner.search(
+    [train_pairs, train_teams], train_labels,
+    validation_data=([val_pairs, val_teams], val_labels),
+    batch_size=30, epochs=20
+)
+
+# 1. Get the best hyperparameters
+best_hp = tuner.get_best_hyperparameters()[0]
+
+# 2. Print the best hyperparameters
+print("Best hyperparameters found:")
+for param, value in best_hp.values.items():
+    print(f"{param}: {value}")
+
+
+
+
+best_hp = tuner.get_best_hyperparameters()[0]
+
+
+best_model = dual_hypermodel.build(best_hp)
+
+
+combined_pairs_trainingSetOne, combined_labels_trainingSetOne, _, team_pairs = trainingSetOne()
+encoded_labels = to_categorical(combined_labels_trainingSetOne, num_classes=30)
+print(combined_labels_trainingSetOne.shape)
+team_pairs = np.array(team_pairs)
+best_model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+best_model.fit([combined_pairs_trainingSetOne, team_pairs], encoded_labels, epochs=20, batch_size=30)
+
+
+
+files = [
+    "../My_datasets/2012_13.xlsx",
+    "../My_datasets/2013_14.xlsx",
+    "../My_datasets/2014_15.xlsx",
+    "../My_datasets/2015_16.xlsx",
+    "../My_datasets/2016_17.xlsx",
+    "../My_datasets/2017_18.xlsx",
+    "../My_datasets/2018_19.xlsx",
+    "../My_datasets/2019_20.xlsx"
+
+]
+
+season_standings = []
+
+# Reading and processing each Excel file
+for file in files:
+    df = pd.read_excel(file)
+    # Extract the team identifier and its position based on index
+    standings_dict = {row['team identifier']: idx + 1 for idx, row in df.iterrows()}
+    season_standings.append(standings_dict)
+
+print(season_standings)
+predictions = best_model.predict([combined_pairs_trainingSetOne, team_pairs])
+# Team identifiers
+team_ids = [11,
+22,
+8,
+3,
+15,
+27,
+14,
+30,
+18,
+5,
+35,
+6,
+7,
+12,
+16,
+23,
+1,
+25,
+21,
+10
+
+]
+# print(team_pairs[:5])
+# Extract and aggregate scores for individual teams from pair predictions
+team_scores = {team_id: 0 for team_id in team_ids}
+for idx, pair in enumerate(team_pairs):
+    team1 = pair[0]
+    team2 = pair[1]
+
+    try:
+        team_scores[team1] += predictions[idx]
+        print(team_scores[team1], ":", predictions[idx])
+        team_scores[team2] += predictions[idx]
+    except KeyError:
+        print(f"Team identifier {team2} not found in team_scores dictionary.")
+        if team2 not in team_ids:
+            print(f"Team identifier {team2} is also not in the original team_ids list.")
+
+# Normalize these aggregated scores to get average probabilities
+total_matches = len(combined_pairs_trainingSetOne) / 2  # Each team appears in half the pairs
+average_probabilities = {team_id: score / total_matches for team_id, score in team_scores.items()}
+
+# Weights for standings
+season_weights = [1, 2, 3, 4, 5, 6, 7, 8]
+weighted_standings = []
+for team_id in team_ids:
+    team_weighted_standing = sum(
+        season_standings[season].get(team_id, len(team_ids) + 1) * season_weights[season] for season in range(8))
+    weighted_standings.append(team_weighted_standing)
+
+# Normalize the weighted standings
+max_weighted_value = len(team_ids) * sum(season_weights)
+normalized_standings = [(max_weighted_value - standing) / max_weighted_value for standing in weighted_standings]
+
+# Weights for combining average probabilities and standings
+w1 = 0.7
+w2 = 0.3
+
+# Adjust the average probabilities
+adjusted_scores = {team_id: w1 * average_probabilities[team_id] + w2 * normalized_standings[team_ids.index(team_id)] for
+                   team_id in team_ids}
+for team_id, score in adjusted_scores.items():
+    print(f"Team ID: {team_id}, Score: {score}")
+
+# Sort teams based on adjusted scores
+scalar_scores = {team_id: np.sum(score) for team_id, score in adjusted_scores.items()}
+
+# Sort teams based on these scalar scores
+sorted_teams = [team for team, _ in sorted(scalar_scores.items(), key=lambda x: x[1], reverse=True)]
+sorted_teams_scores = sorted(scalar_scores.items(), key=lambda x: x[1], reverse=True)
+
+print(sorted_teams_scores)
+print(sorted_teams)
+
+with open('predicted_rankingsTrainingSetThree.txt', 'w') as f:
+    for team in sorted_teams:
+        f.write(f"{team}\n")
---
+++ PRR/creatingPairsForTrainingSetFive.py
@@ -0,0 +1,238 @@
+# Load the data
+from itertools import combinations
+from data_preprocessing import preprocess_data
+import numpy as np
+import pandas as pd
+
+
+def compute_labels_using_rank_difference(data, team_to_rank):
+    pairs = []
+    team_pairs = []
+    labels = []
+    for team1, team2 in combinations(data['team identifier'], 2):
+        pairs.append([
+            data[data['team identifier'] == team1].drop('team identifier', axis=1).values[0],
+            data[data['team identifier'] == team2].drop('team identifier', axis=1).values[0]
+        ])
+        team_pairs.append((team1, team2))
+        rank_team1 = team_to_rank[team1]
+        rank_team2 = team_to_rank[team2]
+        # labels.append(rank_team1 - rank_team2)
+        labels.append(1 if rank_team1 < rank_team2 else 0)
+    pairs = np.array(pairs)
+    labels = np.array(labels)
+    return team_pairs, pairs, labels
+
+
+def trainingSetOne():
+    data = pd.read_excel("../My_datasets/2012_13.xlsx")
+    data_2013_2014 = pd.read_excel("../My_datasets/2013_14.xlsx")
+    data_2014_2015 = pd.read_excel("../My_datasets/2014_15.xlsx")
+    data_2015_2016 = pd.read_excel("../My_datasets/2015_16.xlsx")
+    data_2016_2017 = pd.read_excel("../My_datasets/2016_17.xlsx")
+    data_2017_2018 = pd.read_excel("../My_datasets/2017_18.xlsx")
+    data_2018_2019 = pd.read_excel("../My_datasets/2018_19.xlsx")
+    data_2019_2020 = pd.read_excel("../My_datasets/2019_20.xlsx")
+    data_2020_2021 = pd.read_excel("../My_datasets/2020_21.xlsx")
+    data_2021_2022 = pd.read_excel("../My_datasets/2021_22.xlsx")
+
+
+
+
+
+    # 2012/13
+    numerical_cols_data = data.select_dtypes(include=['int64', 'float64']).columns
+    categorical_cols_data = data.select_dtypes(include=['object']).columns
+    data_preprocessed = preprocess_data(data, numerical_cols_data, categorical_cols_data)
+    data_preprocessed['team identifier'] = data['team identifier']
+    # 2013/14
+    numerical_cols_data_2013_2014 = data_2013_2014.select_dtypes(include=['int64', 'float64']).columns
+    categorical_cols_data_2013_2014 = data_2013_2014.select_dtypes(include=['object']).columns
+    data_preprocessed_data_2013_2014 = preprocess_data(data_2013_2014, numerical_cols_data_2013_2014,
+                                                       categorical_cols_data_2013_2014)
+    data_preprocessed_data_2013_2014['team identifier'] = data_2013_2014['team identifier']
+    # 2014/15
+    numerical_cols_data_2014_2015 = data_2014_2015.select_dtypes(include=['int64', 'float64']).columns
+    categorical_cols_data_2014_2015 = data_2014_2015.select_dtypes(include=['object']).columns
+    data_preprocessed_data_2014_2015 = preprocess_data(data_2014_2015, numerical_cols_data_2014_2015,
+                                                       categorical_cols_data_2014_2015)
+    data_preprocessed_data_2014_2015['team identifier'] = data_2014_2015['team identifier']
+    # 2015/16
+    numerical_cols_data_2015_2016 = data_2015_2016.select_dtypes(include=['int64', 'float64']).columns
+    categorical_cols_data_2015_2016 = data_2015_2016.select_dtypes(include=['object']).columns
+    data_preprocessed_data_2015_2016 = preprocess_data(data_2015_2016, numerical_cols_data_2015_2016,
+                                                       categorical_cols_data_2015_2016)
+    data_preprocessed_data_2015_2016['team identifier'] = data_2015_2016['team identifier']
+    # 2016/17
+    numerical_cols_data_2016_2017 = data_2016_2017.select_dtypes(include=['int64', 'float64']).columns
+    categorical_cols_data_2016_2017 = data_2016_2017.select_dtypes(include=['object']).columns
+    data_preprocessed_data_2016_2017 = preprocess_data(data_2016_2017, numerical_cols_data_2016_2017,
+                                                       categorical_cols_data_2016_2017)
+    data_preprocessed_data_2016_2017['team identifier'] = data_2016_2017['team identifier']
+    # 2017/18
+    numerical_cols_data_2017_2018 = data_2017_2018.select_dtypes(include=['int64', 'float64']).columns
+    categorical_cols_data_2017_2018 = data_2017_2018.select_dtypes(include=['object']).columns
+    data_preprocessed_data_2017_2018 = preprocess_data(data_2017_2018, numerical_cols_data_2017_2018,
+                                                       categorical_cols_data_2017_2018)
+    data_preprocessed_data_2017_2018['team identifier'] = data_2017_2018['team identifier']
+    # 2018/19
+    numerical_cols_data_2018_2019 = data_2018_2019.select_dtypes(include=['int64', 'float64']).columns
+    categorical_cols_data_2018_2019 = data_2018_2019.select_dtypes(include=['object']).columns
+    data_preprocessed_data_2018_2019 = preprocess_data(data_2018_2019, numerical_cols_data_2018_2019,
+                                                       categorical_cols_data_2018_2019)
+    data_preprocessed_data_2018_2019['team identifier'] = data_2018_2019['team identifier']
+    # 2019/20
+    numerical_cols_data_2019_2020 = data_2019_2020.select_dtypes(include=['int64', 'float64']).columns
+    categorical_cols_data_2019_2020 = data_2019_2020.select_dtypes(include=['object']).columns
+    data_preprocessed_data_2019_2020 = preprocess_data(data_2019_2020, numerical_cols_data_2019_2020,
+                                                       categorical_cols_data_2019_2020)
+    data_preprocessed_data_2019_2020['team identifier'] = data_2019_2020['team identifier']
+    # 2020/21
+    numerical_cols_data_2020_2021 = data_2020_2021.select_dtypes(include=['int64', 'float64']).columns
+    categorical_cols_data_2020_2021 = data_2020_2021.select_dtypes(include=['object']).columns
+    data_preprocessed_data_2020_2021 = preprocess_data(data_2020_2021, numerical_cols_data_2020_2021,
+                                                       categorical_cols_data_2020_2021)
+    data_preprocessed_data_2020_2021['team identifier'] = data_2020_2021['team identifier']
+    # 2021/22
+    numerical_cols_data_2021_2022 = data_2021_2022.select_dtypes(include=['int64', 'float64']).columns
+    categorical_cols_data_2021_2022 = data_2021_2022.select_dtypes(include=['object']).columns
+    data_preprocessed_data_2021_2022 = preprocess_data(data_2021_2022, numerical_cols_data_2021_2022,
+                                                       categorical_cols_data_2021_2022)
+    data_preprocessed_data_2021_2022['team identifier'] = data_2021_2022['team identifier']
+
+    #2012/13
+    team_to_rank_2012_13 = {}
+    actual_rankings_2012_13 = data_preprocessed['team identifier'].values
+    for team, rank in zip(data_preprocessed['team identifier'], actual_rankings_2012_13):
+        team_to_rank_2012_13[team] = rank
+
+    # Compute the labels using difference in rankings for 2012/13 season
+    team_pairs, pairs, labels = compute_labels_using_rank_difference(data_preprocessed, team_to_rank_2012_13)
+    flattened_pairs = pairs.reshape(pairs.shape[0], -1)
+
+
+    #2013/14
+    team_to_rank_2013_14 = {}
+    actual_rankings_2013_14 = data_preprocessed_data_2013_2014['team identifier'].values
+    for team, rank in zip(data_preprocessed_data_2013_2014['team identifier'], actual_rankings_2013_14):
+        team_to_rank_2013_14[team] = rank
+
+    # Compute the labels using difference in rankings for 2012/13 season
+    team_pairs_2013_2014, pairs_2013_14, labels_2013_14 = compute_labels_using_rank_difference(data_preprocessed_data_2013_2014, team_to_rank_2013_14)
+    flattened_pairs_2013_14 = pairs_2013_14.reshape(pairs_2013_14.shape[0], -1)
+
+
+    #2014/15
+    team_to_rank_2014_15 = {}
+    actual_rankings_2014_15 = data_preprocessed_data_2014_2015['team identifier'].values
+    for team, rank in zip(data_preprocessed_data_2014_2015['team identifier'], actual_rankings_2014_15):
+        team_to_rank_2014_15[team] = rank
+
+    #Compute the labels using difference in rankings for 2012/13 season
+    team_pairs_2014_2015, pairs_2014_15, labels_2014_15 = compute_labels_using_rank_difference(data_preprocessed_data_2014_2015,
+                                                                                               team_to_rank_2014_15)
+    flattened_pairs_2014_15 = pairs_2014_15.reshape(pairs_2014_15.shape[0], -1)
+
+    #2015/16
+    team_to_rank_2015_16 = {}
+    actual_rankings_2015_16 = data_preprocessed_data_2015_2016['team identifier'].values
+    for team, rank in zip(data_preprocessed_data_2015_2016['team identifier'], actual_rankings_2015_16):
+        team_to_rank_2015_16[team] = rank
+
+    #Compute the labels using difference in rankings for 2012/13 season
+    team_pairs_2015_2016, pairs_2015_16, labels_2015_16 = compute_labels_using_rank_difference(
+        data_preprocessed_data_2015_2016,
+        team_to_rank_2015_16)
+
+    flattened_pairs_2015_16 = pairs_2015_16.reshape(pairs_2015_16.shape[0], -1)
+
+
+    #2016/17
+    team_to_rank_2016_17 = {}
+    actual_rankings_2016_17 = data_preprocessed_data_2016_2017['team identifier'].values
+    for team, rank in zip(data_preprocessed_data_2016_2017['team identifier'], actual_rankings_2016_17):
+        team_to_rank_2016_17[team] = rank
+
+    #Compute the labels using difference in rankings for 2012/13 season
+    team_pairs_2016_2017, pairs_2016_17, labels_2016_17 = compute_labels_using_rank_difference(
+        data_preprocessed_data_2016_2017,
+        team_to_rank_2016_17)
+    flattened_pairs_2016_17 = pairs_2016_17.reshape(pairs_2016_17.shape[0], -1)
+
+    #2017/18
+    team_to_rank_2017_18 = {}
+    actual_rankings_2017_18 = data_preprocessed_data_2017_2018['team identifier'].values
+    for team, rank in zip(data_preprocessed_data_2017_2018['team identifier'], actual_rankings_2017_18):
+        team_to_rank_2017_18[team] = rank
+
+    #Compute the labels using difference in rankings for 2012/13 season
+    team_pairs_2017_2018, pairs_2017_18, labels_2017_18 = compute_labels_using_rank_difference(
+        data_preprocessed_data_2017_2018,
+        team_to_rank_2017_18)
+    flattened_pairs_2017_18 = pairs_2017_18.reshape(pairs_2017_18.shape[0], -1)
+
+    #2018/19
+    team_to_rank_2018_19 = {}
+    actual_rankings_2018_19 = data_preprocessed_data_2018_2019['team identifier'].values
+    for team, rank in zip(data_preprocessed_data_2018_2019['team identifier'], actual_rankings_2018_19):
+        team_to_rank_2018_19[team] = rank
+
+    # Compute the labels using difference in rankings for 2012/13 season
+    team_pairs_2018_2019, pairs_2018_2019, labels_2018_2019 = compute_labels_using_rank_difference(
+        data_preprocessed_data_2018_2019,
+        team_to_rank_2018_19)
+    flattened_pairs_2018_2019 = pairs_2018_2019.reshape(pairs_2018_2019.shape[0], -1)
+
+    # 2019/20
+    team_to_rank_2019_20 = {}
+    actual_rankings_2019_20 = data_preprocessed_data_2019_2020['team identifier'].values
+    for team, rank in zip(data_preprocessed_data_2019_2020['team identifier'], actual_rankings_2019_20):
+        team_to_rank_2019_20[team] = rank
+
+    # Compute the labels using difference in rankings for 2012/13 season
+    team_pairs_2019_2020, pairs_2019_2020, labels_2019_2020 = compute_labels_using_rank_difference(
+        data_preprocessed_data_2019_2020,
+        team_to_rank_2019_20)
+    flattened_pairs_2019_2020 = pairs_2019_2020.reshape(pairs_2019_2020.shape[0], -1)
+
+    # 2020/21
+    team_to_rank_2020_21 = {}
+    actual_rankings_2020_21 = data_preprocessed_data_2020_2021['team identifier'].values
+    for team, rank in zip(data_preprocessed_data_2020_2021['team identifier'], actual_rankings_2020_21):
+        team_to_rank_2020_21[team] = rank
+
+    # Compute the labels using difference in rankings for 2012/13 season
+    team_pairs_2020_2021, pairs_2020_2021, labels_2020_2021 = compute_labels_using_rank_difference(
+        data_preprocessed_data_2020_2021,
+        team_to_rank_2020_21)
+    flattened_pairs_2020_2021 = pairs_2020_2021.reshape(pairs_2020_2021.shape[0], -1)
+
+    # 2021/22
+    team_to_rank_2021_22 = {}
+    actual_rankings_2021_22 = data_preprocessed_data_2021_2022['team identifier'].values
+    for team, rank in zip(data_preprocessed_data_2021_2022['team identifier'], actual_rankings_2021_22):
+        team_to_rank_2021_22[team] = rank
+
+    # Compute the labels using difference in rankings for 2012/13 season
+    team_pairs_2021_2022, pairs_2021_2022, labels_2021_2022 = compute_labels_using_rank_difference(
+        data_preprocessed_data_2021_2022,
+        team_to_rank_2021_22)
+    flattened_pairs_2021_2022 = pairs_2021_2022.reshape(pairs_2021_2022.shape[0], -1)
+
+
+
+    combined_team_pairs = (team_pairs + team_pairs_2013_2014 + team_pairs_2014_2015 + team_pairs_2015_2016 +
+                           team_pairs_2016_2017 + team_pairs_2017_2018+team_pairs_2018_2019+ team_pairs_2019_2020+
+                           team_pairs_2020_2021+ team_pairs_2021_2022)
+
+    combined_pairs_trainingSetOne = np.concatenate((pairs, pairs_2013_14, pairs_2014_15, pairs_2015_16, pairs_2016_17,
+                                                    pairs_2017_18, pairs_2018_2019, pairs_2019_2020, pairs_2020_2021, pairs_2021_2022), axis=0)
+    print("shape from function:    " ,combined_pairs_trainingSetOne.shape)
+    combined_labels_trainingSetOne = np.concatenate((labels, labels_2013_14, labels_2014_15, labels_2015_16, labels_2016_17,
+                                                     labels_2017_18, labels_2018_2019, labels_2019_2020, labels_2020_2021, labels_2021_2022), axis=0)
+    combined_pairs_trainingSetOneXGB = np.concatenate((flattened_pairs, flattened_pairs_2013_14, flattened_pairs_2014_15, flattened_pairs_2015_16,
+                                                       flattened_pairs_2016_17, flattened_pairs_2017_18, flattened_pairs_2018_2019, flattened_pairs_2019_2020,flattened_pairs_2020_2021,
+                                                       flattened_pairs_2021_2022), axis=0)
+
+    return combined_pairs_trainingSetOne, combined_labels_trainingSetOne, combined_pairs_trainingSetOneXGB, combined_team_pairs
+
